0a59b3890c5f2b8a164c35b848d8a9151b434549
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2023-11-17 15:45:52 -0300

    today's work

--- mvpcard/lib/screens/areadelogin.dart
+++ mvpcard/lib/screens/areadelogin.dart
@@ -18,9 +18,48 @@ class Teladelogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
 
       body: 
+
+      Column(
+        children: [
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 96, left: 95, right: 94.94),
+              child: Image.asset(
+                'assets/images/cardapio_web.png',
+              width: 200.6,
+              height: 68,
+              color: const Color(0xffFFB063),
+              
+              ),
+            ),
+          ),
+
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(top: 81, left: 40, right: 27),
+              child: Text(
+                "Qual o nome do seu estabelecimento?",  
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff000000),
+                  fontSize: 17,
+                  letterSpacing: 0.36,
+                ),
+                ),
+            ),
+          ),
+
+          TextField(
+            decoration: InputDecoration(
+           
+            ),
+          ),
+        ],
+      ),
 
 
 
