afd540c4573b201e7e080d38adb9cffc83c5142c
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-11-20 20:18:17 -0300

    today's work

--- lib/screens/areadelogin.dart
+++ lib/screens/areadelogin.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Client extends StatelessWidget {
   const Client({Key? key}) : super(key: key);
 
@@ -18,53 +17,62 @@ class Teladelogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
-      body: 
-
-      Column(
+    return Scaffold(
+      body: Column(
         children: [
-
           Expanded(
             child: Padding(
               padding: const EdgeInsets.only(top: 96, left: 95, right: 94.94),
               child: Image.asset(
                 'assets/images/cardapio_web.png',
-              width: 200.6,
-              height: 68,
-              color: const Color(0xffFFB063),
-              
+                width: 200.6,
+                height: 68,
+                color: const Color(0xffFFB063),
               ),
             ),
           ),
-
           const Expanded(
             child: Padding(
               padding: EdgeInsets.only(top: 81, left: 40, right: 27),
               child: Text(
-                "Qual o nome do seu estabelecimento?",  
+                "Qual o nome do seu estabelecimento?",
                 style: TextStyle(
                   fontWeight: FontWeight.w500,
                   color: Color(0xff000000),
+                  fontFamily: 'Inter',
                   fontSize: 17,
                   letterSpacing: 0.36,
                 ),
-                ),
+              ),
             ),
           ),
-
-          TextField(
-            decoration: InputDecoration(
-           
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(top: 37.0, left: 44, right: 46),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xffB8C2C8),
+                    ),
+                  ),
+                  hintText: "Nome",
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 17,
+                    letterSpacing: -0.41,
+                    fontFamily: 'Inter',
+                    color: Color(0xffB8C2C8),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
       ),
-
-
-
-
-     
     );
   }
 }
